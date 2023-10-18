bce4b0ce42ce30e75323a8507e4c785b5558cdec
jpyrz <130368391+jpyrz@users.noreply.github.com>
2023-10-17 20:18:36 -0500

    chore: adding console log

--- src/scripts/local.js
+++ src/scripts/local.js
@@ -1,6 +1,8 @@
 const drawerToggle = document.getElementById("drawer-toggle");
 const drawer = document.getElementById("drawer");
 
+
 drawerToggle?.addEventListener("click", () => {
+    console.log('opening drawer');
   drawer?.classList.toggle("translate-x-full");
 });
