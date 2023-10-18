bf4bc333e531bc570271a48e7636f72efc70240a
jpyrz <130368391+jpyrz@users.noreply.github.com>
2023-10-17 19:01:24 -0500

    feat: init commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,ts,tsx,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
