79b928c85aa1e843dedf4131aa5b5e4fc3ca20f9
jpyrz <130368391+jpyrz@users.noreply.github.com>
2023-10-17 22:21:39 -0500

    chore: minor responsive changes

--- src/scripts/local.js
+++
@@ -1,8 +0,0 @@
-const drawerToggle = document.getElementById("drawer-toggle");
-const drawer = document.getElementById("drawer");
-
-
-drawerToggle?.addEventListener("click", () => {
-    console.log('opening drawer');
-  drawer?.classList.toggle("translate-x-full");
-});
