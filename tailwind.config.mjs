b0b52a99c548a8cd8d06f7477afec74850115e1d
jpyrz <130368391+jpyrz@users.noreply.github.com>
2023-10-18 22:40:02 -0500

    chore: design changes and implement global styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,ts,tsx,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
+		colors: {
+		  primary: '#FFF', // white
+		  secondary: '#FFC107', // yellow
+		  accent: '#795548', // brown
+		  paletteColor1: '#212B2E', // Gunmetal
+		  paletteColor2: '#383E27', // Black Olive
+		  paletteColor3: '#151F22', // Eerie Black
+		  paletteColor4: '#82754F', // Coyote
+		  paletteColor5: '#D2D4DA' // French Grey
+		},
+      fontFamily: {
+        'base': ['Montserrat', 'sans-serif'],
+      },
+		fontSize: {
+		  '2xl': '1.5rem',
+		  '3xl': '1.875rem',
+		},
+	  },
 	},
 	plugins: [],
-}
+  };
